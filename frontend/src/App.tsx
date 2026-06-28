7ff62cf5cc04e432c7a151e8d04d4b6bfe4ddcb7
Sahil Mohapatra <f20220112@dubai.bits-pilani.ac.in>
2026-06-28 16:47:29 +0530

    reload message updates

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -64,6 +64,7 @@ export default function App() {
 
     const handleBeforeUnload = (e: BeforeUnloadEvent) => {
       e.preventDefault();
+      e.returnValue = "";
     };
 
     const handlePageHide = () => {
